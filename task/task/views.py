120ca71121e5e7cf8088131b19a3ab8c221943d7
danylo5202 <danylo5202@gmail.com>
2021-11-30 03:45:06 +0200

    docker add

--- task/task/views.py
+++ task/task/views.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
-from .models import User,Group
-from django.http import HttpResponse,JsonResponse
+from .models import User, Group
+from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from django.core import serializers
 import json
@@ -8,30 +8,29 @@ from jsonschema import validate
 from .schema import user_schema
 
 
-        
 def user_schema_validation(data):
     try:
-        validate(data,user_schema)
+        validate(data, user_schema)
     except:
         return False
 
-#########  user part
+
+# user part
 def user_add(request):
     user_data = json.loads(request.body)
     print(user_data)
 
     try:
         User.objects.create(
-            username = user_data['username'],
-            email = user_data['email'],
-            is_admin = user_data['is_admin'],
-            date_added = timezone.now()
+            username=user_data['username'],
+            email=user_data['email'],
+            is_admin=user_data['is_admin'],
+            date_added=timezone.now()
         )
     except:
         print('Error in creating user')
         return HttpResponse(status=400)
     return HttpResponse(status=200)
-
 
 
 def get_users(request):
@@ -42,7 +41,7 @@ def get_users(request):
         grps = user.group_set.all()
         grps = [g.name for g in grps]
         u = {
-            'id':user.id,
+            'id': user.id,
             'username': user.username,
             'email': user.email,
             'is_admin': user.is_admin,
@@ -53,7 +52,8 @@ def get_users(request):
     response = json.dumps(response)
     return HttpResponse(response)
 
-def user_edit(request,id):
+
+def user_edit(request, id):
     user_data = json.loads(request.body)
     try:
         user = User.objects.get(id=id)
@@ -64,48 +64,49 @@ def user_edit(request,id):
         user.save()
     except:
         print('error with editing user')
-        return(HttpResponse(status=400))
-    return(HttpResponse(status = 200))
-    
+        return (HttpResponse(status=400))
+    return (HttpResponse(status=200))
 
-def delete_user(request,id):
+
+def delete_user(request, id):
     User.objects.filter(id=id).delete()
     return HttpResponse(status=200)
 
 
-######## group part
+# group part
 
 def add_group(request):
     group_data = json.loads(request.body)
     group = Group(
-        name = group_data['name'],
-        description = group_data['description'],
-        work_names = '0000'
+        name=group_data['name'],
+        description=group_data['description'],
+        work_names='0000'
     )
     group.save()
-    users_to_add = group_data['users_to_add'] # their usernames
-    users = User.objects.filter(id__in = users_to_add)
+    users_to_add = group_data['users_to_add']  # their usernames
+    users = User.objects.filter(id__in=users_to_add)
     for user in users:
         group.users_to_groups.add(user.id)
         # print(user.group_set.all())
-    return JsonResponse(data={'status':200})
+    return JsonResponse(data={'status': 200})
 
 
-def edit_group(request,id):
+def edit_group(request, id):
     try:
         group_data = json.loads(request.body)
         group = Group.objects.get(id=id)
         group.name = group_data['name']
         group.description = group_data['description']
-        users = User.objects.filter(id__in = group_data['group_users'])
+        users = User.objects.filter(id__in=group_data['group_users'])
         group.users_to_groups.clear()
         for user in users:
             group.users_to_groups.add(user.id)
         group.save()
     except Exception:
         print('EXEPTION IN EDITING GROUP')
-        JsonResponse(data={'status':400})
-    return JsonResponse(data={'status':200})
+        JsonResponse(data={'status': 400})
+    return JsonResponse(data={'status': 200})
+
 
 def get_groups(request):
     response = []
@@ -115,7 +116,7 @@ def get_groups(request):
             users = group.users_to_groups.all()
             users = [[u.id, u.username] for u in users]
             grp = {
-                'id':group.id,
+                'id': group.id,
                 'name': group.name,
                 'description': group.description,
                 'work_names': group.work_names,
@@ -125,18 +126,20 @@ def get_groups(request):
         response = json.dumps(response)
     except Exception:
         print('EXEPTION IN RETURNING GROUPS')
-        return(JsonResponse(data={'status': 404}))
+        return (JsonResponse(data={'status': 404}))
     return HttpResponse(response)
 
-def delete_group(request,id):
+
+def delete_group(request, id):
     group = Group.objects.get(id=id)
     if len(group.users_to_groups.all()) > 0:
-        return JsonResponse(data={'response':'Group is not empty'})
+        return JsonResponse(data={'response': 'Group is not empty'})
     try:
         group.delete()
     except:
-        JsonResponse(data={'response':'ERROR'})
-    return JsonResponse(data={'response':'ok'})
+        JsonResponse(data={'response': 'ERROR'})
+    return JsonResponse(data={'response': 'ok'})
+
 
 def set_work_names(request):
     works = json.loads(request.body)
@@ -144,13 +147,4 @@ def set_work_names(request):
     works = ''.join([str(int(w)) for w in works['work_names']])
     group.work_names = works
     group.save()
-    return HttpResponse(status = 200)
-    
-
-
-
-
-
-
-    
-    
+    return HttpResponse(status=200)
